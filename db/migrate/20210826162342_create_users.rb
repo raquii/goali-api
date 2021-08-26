31b8887da11683825ef3ecf51ade4e747134f14a
raquel <raquelanaroman@gmail.com>
2021-08-26 12:28:32 -0400

    create user model/resources

---
+++ db/migrate/20210826162342_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.datetime :birthday
+
+      t.timestamps
+    end
+  end
+end
