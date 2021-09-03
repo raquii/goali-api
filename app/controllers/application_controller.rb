806ea3c10d5fbcfaf65d0cf2234d423c1385e417
raquel <raquelanaroman@gmail.com>
2021-09-03 17:20:26 -0400

    add application controls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::API
+    include ActionController::Cookies
+
+    rescue_from ActiveRecord::RecordNotFound, with: :render_response_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :render_response_invalid
+
+    before_action :authorize
+
+    private
+
+    def authorize
+        @current_user = User.find_by(id: sessions[:user_id])
+        render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
+    end
+
+    def render_response_not_found(execption)
+        render json: { errors: "#{exception.model} not found."}, status: :not_found
+    end
+
+    def render_response_invalid(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
 end
