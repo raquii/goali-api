a31a5356a30aeba24e1609bffec2c1ff999f27f7
raquel <raquelanaroman@gmail.com>
2021-09-03 15:26:40 -0400

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,64 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_26_162342) do
+ActiveRecord::Schema.define(version: 2021_09_03_175620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friend_requests", force: :cascade do |t|
+    t.bigint "requestor_id"
+    t.bigint "receiver_id"
+    t.index ["receiver_id"], name: "index_friend_requests_on_receiver_id"
+    t.index ["requestor_id"], name: "index_friend_requests_on_requestor_id"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "friend_a_id"
+    t.bigint "friend_b_id"
+    t.index ["friend_a_id"], name: "index_friendships_on_friend_a_id"
+    t.index ["friend_b_id"], name: "index_friendships_on_friend_b_id"
+  end
+
+  create_table "habits", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.string "description"
+    t.string "icon"
+    t.string "periodicity"
+    t.integer "goal"
+    t.boolean "private"
+    t.boolean "archived"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_habits_on_user_id"
+  end
+
+  create_table "logs", force: :cascade do |t|
+    t.bigint "habit_id", null: false
+    t.datetime "date"
+    t.index ["habit_id"], name: "index_logs_on_habit_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "habit_id"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["habit_id"], name: "index_messages_on_habit_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "location"
+    t.string "profile_picture"
+    t.string "bio"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -25,4 +79,9 @@ ActiveRecord::Schema.define(version: 2021_08_26_162342) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "habits", "users"
+  add_foreign_key "logs", "habits"
+  add_foreign_key "messages", "habits"
+  add_foreign_key "messages", "users"
+  add_foreign_key "profiles", "users"
 end
