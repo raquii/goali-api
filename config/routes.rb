5fe4a9174437f34b70e24a11a847201962a164f8
raquel <raquelanaroman@gmail.com>
2021-09-03 18:31:40 -0400

    create sign up route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get "/user", to: "users#show"
   delete "/logout", to: "sessions#destroy"
 
+  post "/signup", to: "users#create"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
