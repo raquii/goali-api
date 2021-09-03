42b5461f7fb483992f1fc39cea361e758febaa25
raquel <raquelanaroman@gmail.com>
2021-09-03 13:58:10 -0400

    edit relationships

--- db/migrate/20210903173155_create_messages.rb
+++ db/migrate/20210903173155_create_messages.rb
@@ -1,6 +1,8 @@
 class CreateMessages < ActiveRecord::Migration[6.1]
   def change
     create_table :messages do |t|
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :habit, foreign_key: true
       t.text :content
 
       t.timestamps
