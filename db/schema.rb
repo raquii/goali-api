951b504c8b61343c3d3fc6a12e83140bd2ca50a1
raquel <raquelanaroman@gmail.com>
2021-09-03 17:22:47 -0400

    change birthday to date type

--- db/schema.rb
+++ db/schema.rb
@@ -74,7 +74,7 @@ ActiveRecord::Schema.define(version: 2021_09_03_175620) do
     t.string "name"
     t.string "email"
     t.string "password_digest"
-    t.datetime "birthday"
+    t.date "birthday"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
