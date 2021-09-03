3c29aba681b1b5c5e952b600a15f7594882d7dd1
raquel <raquelanaroman@gmail.com>
2021-09-03 13:58:22 -0400

    edit relationships

--- db/migrate/20210903173407_create_friendships.rb
+++ db/migrate/20210903173407_create_friendships.rb
@@ -1,8 +1,8 @@
 class CreateFriendships < ActiveRecord::Migration[6.1]
   def change
     create_table :friendships do |t|
-
-      t.timestamps
+      t.references :friend_a
+      t.references :friend_b
     end
   end
 end
