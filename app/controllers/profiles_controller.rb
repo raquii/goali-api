1778c9f9bf5ebdb6b50363410a33eaa0d2d0c636
raquel <raquelanaroman@gmail.com>
2021-09-04 17:05:57 -0400

    add show and update method for profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,21 @@
 class ProfilesController < ApplicationController
+    before_action :authorize_editor, only: :update
+
+    def show
+        profile = Profile.find_by(profile_params)
+        render json: profile
+    end 
+
+    def update
+        profile = Profile.find(params[:id])
+        profile.update!(profile_params)
+        render json: profile
+    end
+
+    private
+
+    def profile_params
+        params.permit(:location, :profile_picture, :bio, :user_1)
+    end
+    
 end
