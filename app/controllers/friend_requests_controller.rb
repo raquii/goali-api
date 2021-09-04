bf05365949379a82af0aa9ee78f4306262633177
raquel <raquelanaroman@gmail.com>
2021-09-04 17:06:27 -0400

    add create and destroy for friend requests

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,2 +1,20 @@
 class FriendRequestsController < ApplicationController
+
+    def create
+        request = FriendRequest.create!(friend_request_params)
+        render json: request, status: :created
+    end
+
+    def destroy
+        request = FriendRequest.find_by(friend_request_params)
+        request.destroy
+        render json: {message: "Friend request deleted"}
+    end
+
+    private
+    
+    def friend_request_params
+        params.permit(:requestor_id, :receiver_id)
+    end
+
 end
