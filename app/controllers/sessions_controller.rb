37eca429053d4e7161966bd2be1456ad8553a236
raquel <raquelanaroman@gmail.com>
2021-09-07 19:17:30 -0400

    change to use @current_user variable

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,11 @@ class SessionsController < ApplicationController
     skip_before_action :authorize, only: :create
 
     def create
-        user=User.find_by(username:params[:username])
+        @current_user=User.find_by(username:params[:username])
         
-        if user&.authenticate(params[:password])
-            session[:user_id] = user.id
-            render json: user
+        if @current_user&.authenticate(params[:password])
+            session[:user_id] = @current_user.id
+            render json: @current_user
           else
             render json: { errors: ["Invalid username or password"] }, status: :unauthorized
         end
