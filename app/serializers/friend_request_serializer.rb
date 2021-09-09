e92d97772d63afb970624d23ce9e72598ab9829a
raquel <raquelanaroman@gmail.com>
2021-09-09 13:14:31 -0400

    create serializer for friend request

---
+++ app/serializers/friend_request_serializer.rb
@@ -0,0 +1,9 @@
+class FriendRequestSerializer < ActiveModel::Serializer
+  attributes :id, :username, :name, :profile
+
+  def profile
+    {location: self.object.profile.location,
+      profile_picture: self.object.profile.profile_picture,
+      bio: self.object.profile.bio}
+  end
+end
