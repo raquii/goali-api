f4793d949a6c69acfd56f3eac7c9deef98f2855c
raquel <raquelanaroman@gmail.com>
2021-09-09 13:11:51 -0400

    create logs controller

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,2 +1,25 @@
 class LogsController < ApplicationController
+
+    def create
+        habit = Habit.find(id: params[:habit_id])
+        log = habit.logs.create!(log_params)
+        render json: habit, status: :created
+    end
+
+    def destroy
+        log = find_log
+        log.destroy
+        head :no_content
+    end
+    
+    private
+
+    def log_params
+        params.permit(:date)
+    end
+
+    def find_log
+        log = Log.find(params[:id])
+    end
+    
 end
