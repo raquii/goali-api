e04f033aa9b6a7a2c3929c44d57f3457607718e4
raquel <raquelanaroman@gmail.com>
2021-09-09 13:13:49 -0400

    add validations and helper methods to habit model

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,4 +1,21 @@
 class Habit < ApplicationRecord
   belongs_to :user
   has_many :logs, dependent: :destroy
+
+  validates :name, presence: true, length: {maximum: 60}
+  validates :periodicity, presence: true, inclusion: {in: %w(day week month)}
+  validates :goal, presence: true, numericality: { only_integer: true, less_than: 10 }
+  validates :private, inclusion: [true, false]
+  validates :archived, inclusion: [true, false]
+  
+  def count_times_logged
+    self.logs.count
+  end
+
+  def get_logs
+    self.logs.map do |log|
+      {date: log.date}
+    end
+  end
+
 end
