786925fc812e5294a2ac0de89daa52f0878057e4
raquel <raquelanaroman@gmail.com>
2021-09-09 13:12:27 -0400

    create index for friendship model

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,9 +1,14 @@
 class FriendshipsController < ApplicationController
+    
+    def index
+        friends = @current_user.get_friends
+        render json: friends
+    end
 
     def create
         friendship = Friendship.create!(friendship_params)
         if friendships.valid?
-            request = FriendRequest.lookup(params[:friendship][:friend_a_id], params[:friendship][:friend_b_id])
+            request = FriendRequest.find_by(params[:friendship][:friend_a_id], params[:friendship][:friend_b_id])
             request.destroy
             render json: friendship, status: :created
         end
