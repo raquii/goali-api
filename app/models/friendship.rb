638d9f61fba483f32b9afe04a866d74f385dc0a9
raquel <raquelanaroman@gmail.com>
2021-09-09 13:13:33 -0400

    add validations to friendship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,18 @@
 class Friendship < ApplicationRecord
     belongs_to :friend_a, class_name: :User
     belongs_to :friend_b, class_name: :User
+
+    validates :friend_a_id, presence: true
+    validates :friend_b_id, presence: true
+    validate :request_exists?
+
+    def request_exists?
+       if !FriendRequest.find_request(self.friend_a_id, self.friend_b_id)
+        self.errors.add("Request must exist first")
+       end
+    end
+        
+    def self.find_friendship(friend_a_id, friend_b_id)
+        Friendship.find_by(friend_a_id: friend_a_id, friend_b_id: friend_b_id) || Friendship.find_by(friend_a_id: friend_b_id, friend_b_id: friend_a_id)
+    end
 end
