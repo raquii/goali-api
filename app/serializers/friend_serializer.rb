2aa301d4e811a7d18e69c2958c324a545b6b7361
raquel <raquelanaroman@gmail.com>
2021-09-14 11:40:21 -0400

    add friend serializer

---
+++ app/serializers/friend_serializer.rb
@@ -0,0 +1,25 @@
+class FriendSerializer < ActiveModel::Serializer
+  attributes :friendship_id
+
+  def profile
+    {location: self.object.profile.location,
+      profile_picture: self.object.profile.profile_picture,
+      bio: self.object.profile.bio}
+  end
+
+  def habits
+    self.object.habits.map do |habit|
+      if habit.private == false && habit.archived == false
+        {id: habit.id,
+        name: habit.name,
+        description: habit.description,
+        periodicity: habit.periodicity,
+        frequency: habit.goal,
+        total_times: habit.count_times_logged,
+        logs: habit.get_logs
+        }
+      end
+    end
+  end
+  
+end
