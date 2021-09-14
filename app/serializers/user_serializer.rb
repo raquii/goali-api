ef03b62339ca5c9da2a2040536b22bf96b96cf82
raquel <raquelanaroman@gmail.com>
2021-09-14 13:31:34 -0400

    changing serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,24 +1,12 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :name, :email, :birthday, :profile, :habits
+  attributes :id, :username, :name, :email, :birthday, :profile
+
+  has_many :habits, each_serializer: HabitSerializer
 
   def profile
     {location: self.object.profile.location,
       profile_picture: self.object.profile.profile_picture,
       bio: self.object.profile.bio}
   end
-
-  def habits
-    self.object.habits.map do |habit|
-      {id: habit.id,
-      name: habit.name,
-      description: habit.description,
-      periodicity: habit.periodicity,
-      frequency: habit.goal,
-      private_habit: habit.private,
-      archived: habit.archived,
-      total_times: habit.total_times,
-      logs: habit.get_logs
-      }
-    end
-  end
+  
 end
