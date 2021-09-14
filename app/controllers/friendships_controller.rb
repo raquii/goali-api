36df75bca4c6fe5d8bea8f1508365c185bf453ef
raquel <raquelanaroman@gmail.com>
2021-09-14 11:42:09 -0400

    fix create method

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,14 +1,16 @@
 class FriendshipsController < ApplicationController
-    
+    skip_before_action :authorize
+
     def index
         friends = @current_user.get_friends
-        render json: friends, each_serializer: FriendshipSerializer
+        render json: friends
     end
 
     def create
-        friendship = Friendship.create!(friendship_params)
-        if friendships.valid?
-            request = FriendRequest.find_by(params[:friendship][:friend_a_id], params[:friendship][:friend_b_id])
+        friendship = Friendship.create!(friend_a_id: @current_user.id, friend_b_id: params[:friend_b_id])
+        
+        if friendship.valid?
+            request = FriendRequest.find_request(friendship.friend_a_id, friendship.friend_b_id)
             request.destroy
             render json: friendship, status: :created
         end
